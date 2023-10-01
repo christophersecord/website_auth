6732687105bd22e4a1843f76c0fc59564f22c0c2
csecord <christopher.secord@gmail.com>
2023-10-01 18:40:52 -0500

    change username to email

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,18 +1,23 @@
 
-
+/**
+ * one row per user that is known by the system
+ */
 create table wa_user (
 	userID int not null auto_increment primary key,
-	emailAddress varchar(250) not null,
+	userName varchar(250) not null,
 
 	-- passwd is a SHA-2 hash
 	passwd char(56) not null,
 
 	dateCreated timestamp not null default now(),
 
-	unique(emailAddress)
+	unique(userName)
 );
 
-
+/**
+ * one row per unique login by a user
+ * unique logins are defined by presenting the login token
+ */
 create table wa_login (
 	loginID int not null auto_increment primary key,
 	loginToken char(36) not null,
